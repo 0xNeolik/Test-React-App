dad22fed3a1b3b2f1f0d15643bfa10da6aea34f0
0xNeolik <alvaro.teran@ontheuser.com>
2022-08-01 09:42:58 +0200

    Change the version of react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
-      <h1>Learn React Testing</h1>
+      <h1>App counter Testing</h1>
+      <h2>The count is {counter}</h2>
+      <button onClick={increaseCounter}>Click me to increase</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,9 @@
-import React from "react";
-import App from "./App";
+test("renders without errors", () => {});
 
-test("renders learn react testing", () => {});
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("renders counter start a 0", () => {});
+
+test("renders counter increase when you click the button", () => {});

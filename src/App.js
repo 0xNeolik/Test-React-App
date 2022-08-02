75c0899be525d699f1d3f3578671c7db3558eae0
0xNeolik <alvaro.teran@ontheuser.com>
2022-08-02 21:40:05 +0200

    Create a decrease button and test it

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ function App() {
     setCounter(counter + 1);
   };
 
+  const decreaseCounter = () => {
+    counter !== 0 ? setCounter(counter - 1) : setCounter(0);
+  };
+
   return (
     <div className="App" data-test="component-app">
       <h1>App counter Testing</h1>
       <h2 data-test="component-counter-display">The count is {counter}</h2>
-      <button onClick={increaseCounter} data-test="component-counter-button">
+      <button
+        onClick={increaseCounter}
+        data-test="component-counter-button-increase"
+      >
         Click me to increase
+      </button>
+      <button
+        onClick={decreaseCounter}
+        data-test="component-counter-button-decrease"
+      >
+        Click me to decrease
       </button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -20,7 +20,16 @@ test("renders increment button", () => {
   const wrapper = setup();
   const buttonCounterComponent = findByAttrivute(
     wrapper,
-    "component-counter-button"
+    "component-counter-button-increase"
+  );
+  expect(buttonCounterComponent.length).toBe(1);
+});
+
+test("renders decrease button", () => {
+  const wrapper = setup();
+  const buttonCounterComponent = findByAttrivute(
+    wrapper,
+    "component-counter-button-decrease"
   );
   expect(buttonCounterComponent.length).toBe(1);
 });
@@ -34,6 +43,58 @@ test("renders counter display", () => {
   expect(counterDisplayComponent.length).toBe(1);
 });
 
-test("renders counter start a 0", () => {});
+test("renders counter start a 0", () => {
+  const wrapper = setup();
+  const counterDisplayComponentText = findByAttrivute(
+    wrapper,
+    "component-counter-display"
+  ).text();
+  expect(counterDisplayComponentText).toBe("The count is 0");
+});
 
-test("renders counter increase when you click the button", () => {});
+test("renders counter increase when you click the button", () => {
+  const wrapper = setup();
+  const buttonCounterComponent = findByAttrivute(
+    wrapper,
+    "component-counter-button-increase"
+  );
+  buttonCounterComponent.simulate("click");
+  const counterDisplayComponentText = findByAttrivute(
+    wrapper,
+    "component-counter-display"
+  ).text();
+  expect(counterDisplayComponentText).toBe("The count is 1");
+});
+
+test("renders counter decrease when you click the button", () => {
+  const wrapper = setup();
+  const buttonCounterComponentIncrease = findByAttrivute(
+    wrapper,
+    "component-counter-button-increase"
+  );
+  buttonCounterComponentIncrease.simulate("click");
+  const buttonCounterComponentDecrease = findByAttrivute(
+    wrapper,
+    "component-counter-button-decrease"
+  );
+  buttonCounterComponentDecrease.simulate("click");
+  const counterDisplayComponentText = findByAttrivute(
+    wrapper,
+    "component-counter-display"
+  ).text();
+  expect(counterDisplayComponentText).toBe("The count is 0");
+});
+
+test("renders counter not decrease if counter is 0", () => {
+  const wrapper = setup();
+  const buttonCounterComponentDecrease = findByAttrivute(
+    wrapper,
+    "component-counter-button-decrease"
+  );
+  buttonCounterComponentDecrease.simulate("click");
+  const counterDisplayComponentText = findByAttrivute(
+    wrapper,
+    "component-counter-display"
+  ).text();
+  expect(counterDisplayComponentText).toBe("The count is 0");
+});
